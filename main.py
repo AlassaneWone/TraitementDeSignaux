290e292a5fd21e4d1f1b716c22cadf7aad86c7fd
Alassane Wone <a.wone@students.ephec.be>
2023-10-27 12:03:15 +0200

    détection de formes et excentricité

--- main.py
+++ main.py
@@ -1,96 +1,44 @@
-'''cv2.namedWindow("Camera feed")
-vc = cv2.VideoCapture(0)
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage.color import rgb2gray
+from skimage.draw import polygon_perimeter
+from skimage.measure import label, regionprops
+from scipy.signal import correlate2d, convolve2d
+from skimage.feature import canny
 
-if vc.isOpened():  # try to get the first frame
-    rval, frame = vc.read()
-else:
-    rval = False
+nbr_de_formes = 0
 
-while rval:
-    cv2.imshow("preview", frame)
-    rval, frame = vc.read()
-    key = cv2.waitKey(20)
-    if key == 27:  # exit on ESC
-        break
+image = plt.imread('./shapes.jpg')
 
-vc.release()
-cv2.destroyWindow("preview")
-'''
-# import the necessary packages
-from imutils.video import VideoStream
-import argparse
-import datetime
-import imutils
-import time
-import cv2
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", help="path to the video file")
-ap.add_argument("-a", "--min-area", type=int,
-                default=500, help="minimum area size")
-args = vars(ap.parse_args())
-# if the video argument is None, then we are reading from webcam
-if args.get("video", None) is None:
-    vs = VideoStream(src=0).start()
-    time.sleep(2.0)
-# otherwise, we are reading from a video file
-else:
-    vs = cv2.VideoCapture(args["video"])
-# initialize the first frame in the video stream
-firstFrame = None
-# loop over the frames of the video
-while True:
-    # grab the current frame and initialize the occupied/unoccupied
-    # text
-    frame = vs.read()
-    frame = frame if args.get("video", None) is None else frame[1]
-    text = "Unoccupied"
-    # if the frame could not be grabbed, then we have reached the end
-    # of the video
-    if frame is None:
-        break
-    # resize the frame, convert it to grayscale, and blur it
-    frame = imutils.resize(frame, width=500)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
-    # if the first frame is None, initialize it
-    if firstFrame is None:
-        firstFrame = gray
-        continue
-    # compute the absolute difference between the current frame and
-    # first frame
-    frameDelta = cv2.absdiff(firstFrame, gray)
-    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
-    # dilate the thresholded image to fill in holes, then find contours
-    # on thresholded image
-    thresh = cv2.dilate(thresh, None, iterations=2)
-    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-                            cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
-    # loop over the contours
-    for c in cnts:
-        # if the contour is too small, ignore it
-        if cv2.contourArea(c) < args["min_area"]:
-            continue
-        # compute the bounding box for the contour, draw it on the frame,
-        # and update the text
-        (x, y, w, h) = cv2.boundingRect(c)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        text = "Occupied"
-    # draw the text and timestamp on the frame
-    cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-    cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-                (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-    # show the frame and record if the user presses a key
-    cv2.imshow("Security Feed", frame)
-    cv2.imshow("Thresh", thresh)
-    cv2.imshow("Frame Delta", frameDelta)
-    key = cv2.waitKey(1) & 0xFF
-    # if the `q` key is pressed, break from the lop
-    if key == ord("q"):
-        break
-# cleanup the camera and close any open windows
-vs.stop() if args.get("video", None) is None else vs.release()
-cv2.destroyAllWindows()
-r
+img = rgb2gray(image)
+canny_edges = canny(img, sigma=1)
+
+label_img = label(canny_edges)
+regions = regionprops(label_img)
+
+fig, (ax0, ax1, ax2, ax3) = plt.subplots(1, 4, figsize=(20, 4))
+
+for props in regions:
+    nbr_de_formes += 1
+
+    y0, x0 = props.centroid
+    coords = polygon_perimeter(np.array(props.coords)[:, 0], np.array(props.coords)[:, 1])
+    label_img[coords[0], coords[1]] = 1
+
+    excentricite = props.eccentricity
+    print(f"Forme {nbr_de_formes}: Excentricité = {excentricite}")
+
+    plt.text(x0, y0, str(nbr_de_formes), color='r', fontsize=12, ha='center', va='center')
+
+ax0.imshow(image)
+ax1.imshow(img, cmap='gray')
+ax2.imshow(canny_edges, cmap='gray')
+ax3.imshow(label_img, cmap='gray')
+
+for a in (ax0, ax1, ax2, ax3):
+    a.axis('off')
+
+plt.show()
+
+print(nbr_de_formes)
+
