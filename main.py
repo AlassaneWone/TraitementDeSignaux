363a13901f59de2bc745dfce924540e12fec1981
Alassane Wone <a.wone@students.ephec.be>
2023-11-09 17:33:14 +0100

    détecte la main et les signes
    détecte la main et print si l'utilisateur fait "feuille", "papier", "ciseau" ou "pas de signe détecté". Si aucune main n'est détecté, ne print rien.

--- main.py
+++ main.py
@@ -1,96 +1,51 @@
-'''cv2.namedWindow("Camera feed")
-vc = cv2.VideoCapture(0)
-
-if vc.isOpened():  # try to get the first frame
-    rval, frame = vc.read()
-else:
-    rval = False
-
-while rval:
-    cv2.imshow("preview", frame)
-    rval, frame = vc.read()
-    key = cv2.waitKey(20)
-    if key == 27:  # exit on ESC
-        break
-
-vc.release()
-cv2.destroyWindow("preview")
-'''
-# import the necessary packages
-from imutils.video import VideoStream
-import argparse
-import datetime
-import imutils
-import time
+from cvzone.HandTrackingModule import HandDetector
 import cv2
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", help="path to the video file")
-ap.add_argument("-a", "--min-area", type=int,
-                default=500, help="minimum area size")
-args = vars(ap.parse_args())
-# if the video argument is None, then we are reading from webcam
-if args.get("video", None) is None:
-    vs = VideoStream(src=0).start()
-    time.sleep(2.0)
-# otherwise, we are reading from a video file
-else:
-    vs = cv2.VideoCapture(args["video"])
-# initialize the first frame in the video stream
-firstFrame = None
-# loop over the frames of the video
+
+cap = cv2.VideoCapture(0)
+detector = HandDetector(detectionCon=0.8, maxHands=1)
 while True:
-    # grab the current frame and initialize the occupied/unoccupied
-    # text
-    frame = vs.read()
-    frame = frame if args.get("video", None) is None else frame[1]
-    text = "Unoccupied"
-    # if the frame could not be grabbed, then we have reached the end
-    # of the video
-    if frame is None:
+    # Get image frame
+    success, img = cap.read()
+    # Find the hand and its landmarks
+    hands, img = detector.findHands(img)  # with draw
+
+    if hands:
+        # Information for the first hand detected
+        hand1 = hands[0]  # Get the first hand detected
+        lmList1 = hand1["lmList"]  # List of 21 landmarks for the first hand
+        bbox1 = hand1["bbox"]  # Bounding box around the first hand (x,y,w,h coordinates)
+        center1 = hand1['center']  # Center coordinates of the first hand
+        handType1 = hand1["type"]  # Type of the first hand ("Left" or "Right")
+
+        # Count the number of fingers up for the first hand
+        fingers1 = detector.fingersUp(hand1)
+        # print(f'H1 = {fingers1}', end=" ")  # Print the count of fingers that are up
+
+        if (fingers1[0] == 0 and
+                fingers1[1] == 1 and
+                fingers1[2] == 1 and
+                fingers1[3] == 0 and
+                fingers1[4] == 0):
+            print('ciseaux')
+        elif (fingers1[0] == 0 and
+              fingers1[1] == 0 and
+              fingers1[2] == 0 and
+              fingers1[3] == 0 and
+              fingers1[4] == 0):
+            print('pierre')
+        elif ((fingers1[0] == 1 or fingers1[0] == 0) and
+              fingers1[1] == 1 and
+              fingers1[2] == 1 and
+              fingers1[3] == 1 and
+              fingers1[4] == 1):
+            print('feuille')
+        else:
+            print('pas de signe détecté')
+
+    # Display
+    cv2.imshow("Image", img)
+    if cv2.waitKey(1) == ord('q'):
         break
-    # resize the frame, convert it to grayscale, and blur it
-    frame = imutils.resize(frame, width=500)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
-    # if the first frame is None, initialize it
-    if firstFrame is None:
-        firstFrame = gray
-        continue
-    # compute the absolute difference between the current frame and
-    # first frame
-    frameDelta = cv2.absdiff(firstFrame, gray)
-    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
-    # dilate the thresholded image to fill in holes, then find contours
-    # on thresholded image
-    thresh = cv2.dilate(thresh, None, iterations=2)
-    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-                            cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
-    # loop over the contours
-    for c in cnts:
-        # if the contour is too small, ignore it
-        if cv2.contourArea(c) < args["min_area"]:
-            continue
-        # compute the bounding box for the contour, draw it on the frame,
-        # and update the text
-        (x, y, w, h) = cv2.boundingRect(c)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        text = "Occupied"
-    # draw the text and timestamp on the frame
-    cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-    cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-                (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-    # show the frame and record if the user presses a key
-    cv2.imshow("Security Feed", frame)
-    cv2.imshow("Thresh", thresh)
-    cv2.imshow("Frame Delta", frameDelta)
-    key = cv2.waitKey(1) & 0xFF
-    # if the `q` key is pressed, break from the lop
-    if key == ord("q"):
-        break
-# cleanup the camera and close any open windows
-vs.stop() if args.get("video", None) is None else vs.release()
+
+cap.release()
 cv2.destroyAllWindows()
-r
