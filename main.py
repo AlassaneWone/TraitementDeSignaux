3804d5a3078ab86d0707513737222421c98a152e
Justin Hespel Louis <93406828+JHespelLouis@users.noreply.github.com>
2023-11-28 16:44:01 +0100

    deleted deprecated code

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ import threading
 from cvzone.HandTrackingModule import HandDetector
 import cv2
 
-cap = cv2.VideoCapture(0)
-detector = HandDetector(detectionCon=0.8, maxHands=1)
-
 is_game_running = False
 player_score = 0
 computer_score = 0
@@ -172,46 +169,7 @@ panel = tk.Label(root)
 panel.place(x=700, y=0, width=200, height=600)  # Adjusted the position and size
 
 def video_capture_thread():
-    global cap, detector, user_input
-    while True:
-        # Get image frame
-        success, img = cap.read()
-        # Find the hand and its landmarks
-        hands, img_det = detector.findHands(img)  # with draw
-
-        if hands:
-            # Information for the first hand detected
-            hand1 = hands[0]  # Get the first hand detected
-            lmList1 = hand1["lmList"]  # List of 21 landmarks for the first hand
-            bbox1 = hand1["bbox"]  # Bounding box around the first hand (x,y,w,h coordinates)
-            center1 = hand1['center']  # Center coordinates of the first hand
-            handType1 = hand1["type"]  # Type of the first hand ("Left" or "Right")
-
-            # Count the number of fingers up for the first hand
-            fingers1 = detector.fingersUp(hand1)
-
-            if fingers1 == [0, 1, 1, 0, 0]:
-                user_input = 'scissors'
-                print('ciseaux')
-            elif fingers1 == [0, 0, 0, 0, 0]:
-                user_input = 'rock'
-                print('pierre')
-            elif fingers1 == [0, 1, 1, 1, 1] or fingers1 == [1, 1, 1, 1, 1]:
-                user_input = 'paper'
-                print('feuille')
-            elif fingers1 == [0, 0, 1, 0, 0]:
-                print('Pas cool')
-            else:
-                user_input = None
-                print('pas de signe détecté')
-
-        # Display
-        cv2.imshow("Hand", img_det)
-        if cv2.waitKey(1) == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
+    #Code Alassane
 
 video_thread = threading.Thread(target=video_capture_thread)
 video_thread.daemon = True  # The thread will terminate when the main program terminates
